33db14b49a219d7a02847702c09506f5e6815f8c
tanaton <heiwaboke@gmail.com>
2010-05-03 02:47:20 +0900

    sscanf、コメント修正

--- unstring.c
+++ unstring.c
@@ -9,16 +9,16 @@ static void *unstr_malloc(size_t size);
 static void *unstr_realloc(void *p, size_t size, size_t len);
 static unstr_bool_t unstr_check_heap_size(unstr_t *str, size_t size);
 
-//! メモリを確保し領域をしるしで埋める。
-/*!
- * \param[in] size 確保する領域のサイズ
- * \return 確保した領域へのポインタ
+/**
+ * @brief		メモリを確保し領域をしるしで埋める。
+ * @param[in]	size	確保する領域のサイズ
+ * @return		確保した領域へのポインタ
  */
 static void *unstr_malloc(size_t size)
 {
 	void *p = malloc(size);
 	if(p == NULL){
-		/*! 領域の確保に失敗した場合、perrorを呼び出す。 */
+		/* 領域の確保に失敗した場合、perrorを呼び出す。 */
 		perror("unstr_malloc:");
 	} else {
 		memset(p, UNSTRING_MEMORY_STAMP, size);
@@ -26,18 +26,18 @@ static void *unstr_malloc(size_t size)
 	return p;
 }
 
-//! 領域を拡張し、新しい領域をしるしで埋める。
-/*!
- * \param[in] p 領域へのポインタ
- * \param[in] size 新しい領域のサイズ
- * \param[in] len 元々使用していた領域のサイズ
- * \return 拡張した領域へのポインタ
+/**
+ * @brief		領域を拡張し、新しい領域をしるしで埋める。
+ * @param[in]	p		領域へのポインタ
+ * @param[in]	size	新しい領域のサイズ
+ * @param[in]	len		元々使用していた領域のサイズ
+ * @return		拡張した領域へのポインタ
  */
 static void *unstr_realloc(void *p, size_t size, size_t len)
 {
 	p = realloc(p, size);
 	if(p == NULL){
-		/*! 領域の確保に失敗した場合、perrorを呼び出す。 */
+		/* 領域の確保に失敗した場合、perrorを呼び出す。 */
 		perror("unstr_realloc:");
 	} else {
 		len++;
@@ -48,23 +48,25 @@ static void *unstr_realloc(void *p, size_t size, size_t len)
 	return p;
 }
 
-//! 文字列を拡張する際に領域の確保が必要か計算する
-/*!
- * \param[in] str 計算を行う文字列
- * \param[in] size 拡張する文字列の長さ
- * \return UNSTRING_TRUE:必要、UNSTRING_FALSE:不要
+/**
+ * @brief		文字列を拡張する際に領域の確保が必要か計算する
+ * @param[in]	str		計算を行う文字列
+ * @param[in]	size	拡張する文字列の長さ
+ * @return		判定結果
+ * @return		UNSTRING_TRUE	必要
+ * @return		UNSTRING_FALSE	不要
  */
 static unstr_bool_t unstr_check_heap_size(unstr_t *str, size_t size)
 {
 	return (((str->length + size) >= str->heap) ? UNSTRING_TRUE : UNSTRING_FALSE);
 }
 
-//! 文字列のバッファを拡張する
-/*!
- * \param[in] str 拡張対象
- * \param[in] size 増加させる量
- * \return 無し
- * \public
+/**
+ * @brief		文字列のバッファを拡張する
+ * @param[in]	str		拡張対象
+ * @param[in]	size	増加させる量
+ * @return		無し
+ * @public
  */
 unstr_t *unstr_alloc(unstr_t *str, size_t size)
 {
@@ -74,7 +76,7 @@ unstr_t *unstr_alloc(unstr_t *str, size_t size)
 		str->heap = 0;
 		str->data = NULL;
 	}
-	/*! 頻繁に確保すると良くないらしいので大まかに確保して \n
+	/* 頻繁に確保すると良くないらしいので大まかに確保して
 	 * 確保する回数を減らす。
 	 */
 	str->heap += ((size / UNSTRING_HEAP_SIZE) + 1) * UNSTRING_HEAP_SIZE;
@@ -82,11 +84,11 @@ unstr_t *unstr_alloc(unstr_t *str, size_t size)
 	return str;
 }
 
-//! 引数で渡された文字列をunstr_tで初期化する
-/*!
- * \param[in] str 文字列
- * \return unstr_tに変換された文字列
- * \public
+/**
+ * @brief		引数で渡された文字列をunstr_tで初期化する
+ * @param[in]	str		文字列
+ * @return		unstr_tに変換された文字列
+ * @public
  */
 unstr_t *unstr_init(const char *str)
 {
@@ -98,11 +100,11 @@ unstr_t *unstr_init(const char *str)
 	return data;
 }
 
-//! 文字列の領域だけ確保して初期化する。
-/*!
- * \param[in] size 確保するunstr_t型のバッファサイズ
- * \return 空のunstr_t型
- * \public
+/**
+ * @brief		文字列の領域だけ確保して初期化する。
+ * @param[in]	size	確保するunstr_t型のバッファサイズ
+ * @return		空のunstr_t型
+ * @public
  */
 unstr_t *unstr_init_memory(size_t size)
 {
@@ -113,11 +115,11 @@ unstr_t *unstr_init_memory(size_t size)
 	return data;
 }
 
-//! 文字列(unstr_t)を開放する
-/*!
- * \param[in] str 開放するunstr_t型
- * \return 無し
- * \public
+/**
+ * @brief		文字列(unstr_t)を開放する
+ * @param[in]	str		開放するunstr_t型
+ * @return		無し
+ * @public
  */
 void unstr_free_func(unstr_t *str)
 {
@@ -130,12 +132,12 @@ void unstr_free_func(unstr_t *str)
 	free(str);
 }
 
-//! 引数で渡された文字列を開放する
-/*!
- * \param[in] size 開放する文字列(unstr_t)の数
- * \param[in] ... 開放する文字列(unstr_t)
- * \return 無し
- * \public
+/**
+ * @brief		引数で渡された文字列を開放する
+ * @param[in]	size	開放する文字列(unstr_t)の数
+ * @param[in]	...		開放する文字列(unstr_t)
+ * @return		無し
+ * @public
  */
 void unstr_delete(size_t size, ...)
 {
@@ -149,11 +151,11 @@ void unstr_delete(size_t size, ...)
 	va_end(list);
 }
 
-//! 空文字列で初期化する。領域は開放しない。
-/*!
- * \param[in,out] str 初期化する文字列
- * \return 無し
- * \public
+/**
+ * @brief			空文字列で初期化する。領域は開放しない。
+ * @param[in,out]	str		初期化する文字列
+ * @return			無し
+ * @public
  */
 void unstr_zero(unstr_t *str)
 {
@@ -165,11 +167,13 @@ void unstr_zero(unstr_t *str)
 	}
 }
 
-//! 文字列用の領域が確保されていることを確認する。
-/*!
- * \param[in] str 対象文字列
- * \return 確保済み:UNSTRING_TRUE, 確保なし:UNSTRING_FALSE
- * \public
+/**
+ * @brief		文字列用の領域が確保されていることを確認する。
+ * @param[in]	str		対象文字列
+ * @return		確認結果
+ * @return		UNSTRING_TRUE	確保済み
+ * @return		UNSTRING_FALSE	確保なし
+ * @public
  */
 unstr_bool_t unstr_isset(unstr_t *str)
 {
@@ -182,11 +186,13 @@ unstr_bool_t unstr_isset(unstr_t *str)
 	return ret;
 }
 
-//! 文字列が空またはNULLであるか確認する。
-/*!
- * \param[in] str 対象文字列
- * \return 空:UNSTRING_TRUE, 非空:UNSTRING_FALSE
- * \public
+/**
+ * @brief		文字列が空またはNULLであるか確認する。
+ * @param[in]	str		対象文字列
+ * @return		確認結果
+ * @return		UNSTRING_TRUE	空
+ * @return		UNSTRING_FALSE	空ではない
+ * @public
  */
 unstr_bool_t unstr_empty(unstr_t *str)
 {
@@ -199,11 +205,11 @@ unstr_bool_t unstr_empty(unstr_t *str)
 	return ret;
 }
 
-//! 文字列の長さを返す
-/*!
- * \param[in] str 対象文字列
- * \return 文字列の長さ
- * \public
+/**
+ * @brief		文字列の長さを返す
+ * @param[in]	str		対象文字列
+ * @return		文字列の長さ
+ * @public
  */
 size_t unstr_strlen(unstr_t *str)
 {
@@ -213,11 +219,11 @@ size_t unstr_strlen(unstr_t *str)
 	return 0;
 }
 
-//! 文字列のコピーを返す。領域の大きさもある程度揃える
-/*!
- * \param[in] str コピー元
- * \return コピーした文字列
- * \public
+/**
+ * @brief		文字列のコピーを返す。領域の大きさもある程度揃える
+ * @param[in]	str		コピー元
+ * @return		コピーした文字列
+ * @public
  */
 unstr_t *unstr_copy(unstr_t *str)
 {
@@ -229,12 +235,14 @@ unstr_t *unstr_copy(unstr_t *str)
 	return data;
 }
 
-//! 文字列をコピーする。
-/*!
- * \param[in,out] s1 コピー先
- * \param[in] s2 コピー元
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			文字列をコピーする。
+ * @param[in,out]	s1		コピー先
+ * @param[in]		s2		コピー元
+ * @return			コピー結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_strcpy(unstr_t *s1, unstr_t *s2)
 {
@@ -245,12 +253,14 @@ unstr_bool_t unstr_strcpy(unstr_t *s1, unstr_t *s2)
 	return UNSTRING_FALSE;
 }
 
-//! unstr_t文字列にchar文字列をコピーする。
-/*!
- * \param[in,out] s1 コピー先
- * \param[in] s2 コピー元
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			unstr_t文字列にchar文字列をコピーする。
+ * @param[in,out]	s1		コピー先
+ * @param[in]		s2		コピー元
+ * @return			コピー結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_strcpy_char(unstr_t *s1, const char *s2)
 {
@@ -264,13 +274,15 @@ unstr_bool_t unstr_strcpy_char(unstr_t *s1, const char *s2)
 	return UNSTRING_TRUE;
 }
 
-//! 文字列を切り出してコピーする
-/*!
- * \param[in,out] s1 コピー先
- * \param[in] s2 コピー元
- * \param[in] size コピーする長さ
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			文字列を切り出してコピーする
+ * @param[in,out]	s1		コピー先
+ * @param[in]		s2		コピー元
+ * @param[in]		size	コピーする長さ
+ * @return			コピー結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_substr(unstr_t *s1, unstr_t *s2, size_t len)
 {
@@ -288,12 +300,14 @@ unstr_bool_t unstr_substr(unstr_t *s1, unstr_t *s2, size_t len)
 	return UNSTRING_TRUE;
 }
 
-//! char文字列を切り出してunstr_t文字列に変換する
-/*!
- * \param[in] str 対象文字列
- * \param[in] size コピーする長さ
- * \return unstr_t文字列
- * \public
+/**
+ * @brief		char文字列を切り出してunstr_t文字列に変換する
+ * @param[in]	str		対象文字列
+ * @param[in]	size	コピーする長さ
+ * @return		変換結果
+ * @return		UNSTRING_TRUE	成功
+ * @return		UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_t *unstr_substr_char(const char *str, size_t len)
 {
@@ -307,12 +321,14 @@ unstr_t *unstr_substr_char(const char *str, size_t len)
 	return data;
 }
 
-//! 文字列を結合する
-/*!
- * \param[in,out] s1 結合先文字列
- * \param[in] s2 結合文字列
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			文字列を結合する
+ * @param[in,out]	s1		結合先文字列
+ * @param[in]		s2		結合文字列
+ * @return			変換結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_strcat(unstr_t *s1, unstr_t *s2)
 {
@@ -326,12 +342,14 @@ unstr_bool_t unstr_strcat(unstr_t *s1, unstr_t *s2)
 	return UNSTRING_TRUE;
 }
 
-//! unstr_t文字列にchar文字列を結合する
-/*!
- * \param[in,out] str 結合先文字列
- * \param[in] c 結合文字列
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			unstr_t文字列にchar文字列を結合する
+ * @param[in,out]	str		結合先文字列
+ * @param[in]		c		結合文字列
+ * @return			結合結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_strcat_char(unstr_t *str, const char *c)
 {
@@ -344,12 +362,15 @@ unstr_bool_t unstr_strcat_char(unstr_t *str, const char *c)
 	return ret;
 }
 
-//! 文字列を比較する
-/*!
- * \param[in] s1 比較文字列1
- * \param[in] s2 比較文字列2
- * \return エラー時に0x100を返す。他はstrcmp関数と同じ
- * \public
+/**
+ * @brief		文字列を比較する
+ * @param[in]	s1		比較文字列1
+ * @param[in]	s2		比較文字列2
+ * @return		比較結果
+ * @return		0			同じ
+ * @return		0x100		エラー
+ * @return		上記以外	違う(文字コードの差分)
+ * @public
  */
 int unstr_strcmp(unstr_t *s1, unstr_t *s2)
 {
@@ -359,13 +380,13 @@ int unstr_strcmp(unstr_t *s1, unstr_t *s2)
 	return 0x100;
 }
 
-//! 対象文字列を区切り文字で切り、格納先に格納する。
-/*!
- * \param[in] str 対象文字列
- * \param[in] tmp 区切り文字列
- * \param[out] len 配列の長さ
- * \return unstr_tの配列
- * \public
+/**
+ * @brief		対象文字列を区切り文字で切り、格納先に格納する。
+ * @param[in]	str		対象文字列
+ * @param[in]	tmp		区切り文字列
+ * @param[out]	len		配列の長さ
+ * @return		unstr_tの配列
+ * @public
  */
 unstr_t **unstr_split(unstr_t *str, const char *tmp, size_t *len)
 {
@@ -397,13 +418,13 @@ unstr_t **unstr_split(unstr_t *str, const char *tmp, size_t *len)
 	return ret;
 }
 
-//! 自動拡張機能付きsprintf。細かいフォーマットには未対応。
-/*!
- * \param[in,out] str 格納先
- * \param[in] format フォーマット
- * \param[in] ... 可変引数
- * \return unstr_t文字列
- * \public
+/**
+ * @brief			自動拡張機能付きsprintf。細かいフォーマットには未対応。
+ * @param[in,out]	str		格納先
+ * @param[in]		format	フォーマット
+ * @param[in]		...		可変引数
+ * @return			unstr_t文字列
+ * @public
  */
 unstr_t *unstr_sprintf(unstr_t *str, const char *format, ...)
 {
@@ -472,11 +493,13 @@ unstr_t *unstr_sprintf(unstr_t *str, const char *format, ...)
 	return str;
 }
 
-//! 文字列を反転させる。破壊的。
-/*!
- * \param[in,out] str 対象文字列
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief			文字列を反転させる。破壊的。
+ * @param[in,out]	str		対象文字列
+ * @return			反転結果
+ * @return			UNSTRING_TRUE	成功
+ * @return			UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_reverse(unstr_t *str)
 {
@@ -495,12 +518,12 @@ unstr_bool_t unstr_reverse(unstr_t *str)
 	return UNSTRING_TRUE;
 }
 
-//! 数値から文字列を作成する。
-/*!
- * \param[in] num 対象数値
- * \param[in] physics 基数
- * \return unstr_t文字列
- * \public
+/**
+ * @brief		数値から文字列を作成する。
+ * @param[in]	num		対象数値
+ * @param[in]	physics	基数
+ * @return		unstr_t文字列
+ * @public
  */
 unstr_t *unstr_itoa(int num, size_t physics)
 {
@@ -546,13 +569,15 @@ unstr_t *unstr_itoa(int num, size_t physics)
 	return str;
 }
 
-//! 頭の良いsubstr
-/*!
- * \param[in] data 対象文字列
- * \param[in] format 切り出すフォーマット
- * \param[out] ... 領域が確保されたunstr_t型
- * \return 切り出した文字数を返す
- * \public
+/**
+ * @brief		頭の良いsubstr
+ * @param[in]	data	対象文字列
+ * @param[in]	format	切り出すフォーマット
+ * @param[out]	...		領域が確保されたunstr_t型
+ * @return		切り出した文字数
+ * @public
+ *
+ * @par			詳細:
  * dataからformat通りに文字列を切り出し、unstr_t型に格納する。\n
  * 対応フォーマットは「$」のみ\n
  * "abcdefg"から"cd"と"fg"を抜き出したい場合、"ab$e$"と書く\n
@@ -570,24 +595,24 @@ size_t unstr_sscanf(unstr_t *data, const char *format, ...)
 	char *index = 0;
 
 	if(!unstr_isset(data)) return 0;
+	if(format == NULL) return 0;
 	search = unstr_init_memory(UNSTRING_HEAP_SIZE);
 	va_start(list, format);
 	/* 先頭を探索する */
-	if((*format != '$') && (*format != '\0')){
-		do {
-			if(unstr_check_heap_size(search, 1)){
-				unstr_alloc(search, 1);
-			}
-			search->data[search->length++] = *format++;
-		} while((*format != '$') && (*format != '\0'));
-		search->data[search->length] = '\0';
-		index = strstr(tmp, search->data);
-		if(index == NULL){
-			tmp += strlen(tmp);
-			format += strlen(format);
-		} else {
-			tmp = index + search->length;
+	while((*format != '$') && (*format != '\0')){
+		if(unstr_check_heap_size(search, 1)){
+			unstr_alloc(search, 1);
 		}
+		search->data[search->length++] = *format++;
+	}
+	search->data[search->length] = '\0';
+	/* search->dataが長さ0の文字列の場合、tmpを返す */
+	index = strstr(tmp, search->data);
+	if(index == NULL){
+		tmp += strlen(tmp);
+		format += strlen(format);
+	} else {
+		tmp = index + search->length;
 	}
 	while(*format != '\0'){
 		if(*format++ != '$') continue;
@@ -636,11 +661,11 @@ size_t unstr_sscanf(unstr_t *data, const char *format, ...)
 	return count;
 }
 
-//! ファイルを丸ごと読み込む
-/*!
- * \param[in] filename ファイルパス
- * \return 読み込んだファイルの中身
- * \public
+/**
+ * @brief		ファイルを丸ごと読み込む
+ * @param[in]	filename	ファイルパス
+ * @return		読み込んだファイルの中身
+ * @public
  */
 unstr_t *unstr_file_get_contents(unstr_t *filename)
 {
@@ -670,13 +695,15 @@ unstr_t *unstr_file_get_contents(unstr_t *filename)
 	return str;
 }
 
-//! ファイルに書き出す
-/*!
- * \param[in] filename ファイルパス
- * \param[in] data 書き込む内容
- * \param[in] mode 書き込みモード
- * \return 成功:UNSTRING_TRUE, 失敗:UNSTRING_FALSE
- * \public
+/**
+ * @brief		ファイルに書き出す
+ * @param[in]	filename	ファイルパス
+ * @param[in]	data		書き込む内容
+ * @param[in]	mode		書き込みモード
+ * @return		出力結果
+ * @return		UNSTRING_TRUE	成功
+ * @return		UNSTRING_FALSE	失敗
+ * @public
  */
 unstr_bool_t unstr_file_put_contents(unstr_t *filename, unstr_t *data, const char *mode)
 {
@@ -695,13 +722,13 @@ unstr_bool_t unstr_file_put_contents(unstr_t *filename, unstr_t *data, const cha
 	return UNSTRING_TRUE;
 }
 
-//! 文字列を置換する。非破壊。
-/*!
- * \param[in] data 対象文字列
- * \param[in] search 置換対象文字列
- * \param[in] replace 置換文字列
- * \return 対象文字列から置換対象文字列を置換文字列に置換した文字列
- * \public
+/**
+ * @brief		文字列を置換する。非破壊。
+ * @param[in]	data	対象文字列
+ * @param[in]	search	置換対象文字列
+ * @param[in]	replace	置換文字列
+ * @return		対象文字列から置換対象文字列を置換文字列に置換した文字列
+ * @public
  */
 unstr_t *unstr_replace(unstr_t *data, unstr_t *search, unstr_t *replace)
 {
@@ -737,13 +764,13 @@ unstr_t *unstr_replace(unstr_t *data, unstr_t *search, unstr_t *replace)
 	return string;
 }
 
-//! クイックサーチのお試し実装
-/*!
- * \param[in] text 対象文字列
- * \param[in] search 検索文字列
- * \param[out] size 戻り値配列の要素数
- * \return 検索文字列に一致した文字列の先頭要素をまとめた配列
- * \public
+/**
+ * @brief		クイックサーチのお試し実装
+ * @param[in]	text	対象文字列
+ * @param[in]	search	検索文字列
+ * @param[out]	size	戻り値配列の要素数
+ * @return		検索文字列に一致した文字列の先頭要素をまとめた配列
+ * @public
  */
 size_t *unstr_quick_search(unstr_t *text, unstr_t *search, size_t *size)
 {
@@ -783,13 +810,15 @@ size_t *unstr_quick_search(unstr_t *text, unstr_t *search, size_t *size)
 	return array;
 }
 
-//! 文字列をトークンで切り分ける。マルチスレッド対応？
-/*!
- * \param[in,out] str 対象文字列。改変されます。
- * \param[in] delim トークン(文字列可)
- * \param[in,out] index インデックス値。次回呼び出し時に必要
- * \return 切り出した文字列
- * \public
+/**
+ * @brief			文字列をトークンで切り分ける
+ * @param[in,out]	str		対象文字列。改変されます。
+ * @param[in]		delim	トークン(文字列可)
+ * @param[in,out]	index	インデックス値。次回呼び出し時に必要
+ * @return			切り出した文字列
+ * @public
+ *
+ * @par				詳細:
  * delimで指定された「文字列」でstrを切り分けます。
  */
 unstr_t *unstr_strtok(unstr_t *str, const char *delim, size_t *index)
